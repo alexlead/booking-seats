725b2f9834e16a30c41fabe96118a8f247d5eba5
Alexander Lead <a.lead@yandex.ru>
2024-02-13 10:00:27 +0300

    fix(version): updated version and author contactsy

--- alexlead_wp_dubrovnik_booking_form/alexlead_wp_dubrovnik_booking_form.php
+++ alexlead_wp_dubrovnik_booking_form/alexlead_wp_dubrovnik_booking_form.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: alexlead-wp-dubrovnik-booking-form 
-Plugin URI: http://runwave.ru
+Plugin URI: https://codepen.io/alexlead/
 Description: Booking form pugin. The plugin build booking form on front page. Please set short code [booking_calendar] on page for booking seats. For confirming|deleting bookings please make page with short code [booking_confimation_page]. Register confirming page on options page. Please check all data on the options page. Set total seats quantity (more than zero, less than 10000). Function for auto sending reminding letter "ALWPDBF_daily_reminder" - register it in wp-cron.
-Version: 1.0
+Version: 1.0.1
 Author: Alex Lead
-Author URI: http://runwave.ru
+Author URI: https://codepen.io/alexlead/
 License: Special issue by customer order
 
 */
@@ -15,34 +15,37 @@ if (!defined('ABSPATH')) exit;
 
 //define base paths
 
-if ( ! defined( 'ALWPDBF_PLUGIN_DIR' ) ) {
-	define( 'ALWPDBF_PLUGIN_DIR', untrailingslashit( dirname( __FILE__ ) ) );
+if (!defined('ALWPDBF_PLUGIN_DIR')) {
+    define('ALWPDBF_PLUGIN_DIR', untrailingslashit(dirname(__FILE__)));
 }
-if ( ! defined( 'ALWPDBF_PLUGIN_URL' ) ) {
-	define( 'ALWPDBF_PLUGIN_URL', untrailingslashit( plugins_url( '', __FILE__ ) ) );
+if (!defined('ALWPDBF_PLUGIN_URL')) {
+    define('ALWPDBF_PLUGIN_URL', untrailingslashit(plugins_url('', __FILE__)));
 }
 
 //for test only
 //define('AL_WP_BOOKING_FORM_BASEURL','http://test1.ru/wordpress/wp-content/plugins/admin/');
 
 /* -- load installation file and start instalation procedure -- */
-function installing(){
-    require_once ALWPDBF_PLUGIN_DIR . '/install/install.php'; 
+function installing()
+{
+    require_once ALWPDBF_PLUGIN_DIR . '/install/install.php';
     install_database();
 }
 
 /* -- load uninstalling file and start uninstalling procedure -- */
-function uninstalling(){
+function uninstalling()
+{
     require_once ALWPDBF_PLUGIN_DIR . '/install/uninstall.php';
     uninstall_database();
 }
 
 
 
-register_activation_hook( __FILE__, 'installing');
-register_deactivation_hook( __FILE__, 'uninstalling');
+register_activation_hook(__FILE__, 'installing');
+register_deactivation_hook(__FILE__, 'uninstalling');
 
-function sendMails() { 
+function sendMails()
+{
     echo 'something';
 }
 
@@ -51,15 +54,15 @@ add_action('ALWPDBF_wp_reminder', 'sendMails');
 
 
 /* --- New configuration --- */
-    
-require_once untrailingslashit( dirname( __FILE__ ) ).'/config/paths-variables.php';
+
+require_once untrailingslashit(dirname(__FILE__)) . '/config/paths-variables.php';
 $ALWPDBF_vars = new ALWPDBF_vars(ALWPDBF_PLUGIN_DIR, ALWPDBF_PLUGIN_URL);
 
 
 require_once $ALWPDBF_vars->config() . 'admin-tools.php';
 
 require_once $ALWPDBF_vars->config() . 'view-form-conf.php';
-    
+
 /*  --  Configuration load  --   */
 require_once $ALWPDBF_vars->classes() . 'class-files-inc.php';
 $configGet = new ALWPDBF_files($ALWPDBF_vars->basicConfig());
@@ -67,7 +70,3 @@ $configArray = $configGet->getData();
 $totalSeatsOnBoat = $configArray[0];
 $currentAdminLang = $configArray[1];
 $currentFrontLang = $configArray[2];
-
-
-
-?>
